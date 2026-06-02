fadd4a68160c397b88f8b0c6009ff3b0952558c6
Richard <62250275+richard-post-ams@users.noreply.github.com>
2026-06-02 15:45:31 +0200

    debug: add console.log to A-B loop patch

--- js/groove_writer.js
+++ js/groove_writer.js
@@ -2725,6 +2725,9 @@ function GrooveWriter() {
 				if (window.ABLoop && window.ABLoop.isActive()) {
 					ab_start = window.ABLoop.getStart() - 1;
 					ab_end   = window.ABLoop.getEnd();
+					console.log('[AB] active: start=' + ab_start + ' end=' + ab_end);
+				} else {
+					console.log('[AB] not active. isActive=' + (window.ABLoop ? window.ABLoop.isActive() : 'ABLoop missing'));
 				}
 
 				// First measure of the range
